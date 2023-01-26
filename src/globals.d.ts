fbc83950f3159826cace85e3acf5fa9113d09e9b
Ramzi Oueslati <ramzi.oueslati@ensi-uma.tn>
2023-01-26 09:12:14 +0100

    chore: use iktos rdkit-provider
    See #7

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,9 +1,7 @@
 /* eslint-disable no-var */
-import { RDKitModule } from '@rdkit/rdkit';
 
 export {};
 
 declare global {
-  var initRDKitModule: (() => Promise<RDKitModule>) | null;
   var moleculeRepresentationSVGCache: Record<string, string> | null;
 }
