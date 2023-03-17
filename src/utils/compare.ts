6597c365f6b1936808c7d9ad9dbcf4cd65fccff0
Ramzi Oueslati <ramzi.oueslati@ensi-uma.tn>
2023-03-17 09:23:37 +0100

    fix: deep compare props in should component update, to avoid costly rerenders
    See #52

---
+++ src/utils/compare.ts
@@ -0,0 +1,56 @@
+export const isEqual = (obj1: unknown, obj2: unknown): boolean => {
+  // Get the type of each object
+  const type1 = typeof obj1;
+  const type2 = typeof obj2;
+
+  // If the types are different, the objects are not equal
+  if (type1 !== type2) {
+    return false;
+  }
+
+  // Handle specific types
+  switch (type1) {
+    case 'object':
+      // Compare arrays and objects
+      if (Array.isArray(obj1) && Array.isArray(obj2)) {
+        // Compare arrays
+        if (obj1.length !== obj2.length) {
+          return false;
+        }
+        for (let i = 0; i < obj1.length; i++) {
+          if (!isEqual(obj1[i], obj2[i])) {
+            return false;
+          }
+        }
+      } else if (obj1 instanceof Date && obj2 instanceof Date) {
+        // Compare dates
+        if (obj1.getTime() !== obj2.getTime()) {
+          return false;
+        }
+      } else if (obj1 && obj2) {
+        // Compare objects
+        const keys1 = Object.keys(obj1);
+        const keys2 = Object.keys(obj2);
+        if (keys1.length !== keys2.length) {
+          return false;
+        }
+        for (const key of keys1) {
+          // @ts-ignore
+          // eslint-disable-next-line no-prototype-builtins
+          if (!obj2.hasOwnProperty(key) || !isEqual(obj1[key], obj2[key])) {
+            return false;
+          }
+        }
+      }
+      break;
+    default:
+      // Compare primitives and functions
+      if (obj1 !== obj2) {
+        return false;
+      }
+      break;
+  }
+
+  // If all tests passed, the objects are equal
+  return true;
+};
